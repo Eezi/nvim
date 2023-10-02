90809508123e67bc1690a1aee9880010fa557232
Eezi <tomi.a.salo@outlook.com>
2023-10-02 19:33:37 +0300

    Removed readme

--- init.lua
+++ init.lua
@@ -376,6 +376,7 @@ vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end)
 vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end)
 -------------------------------------
+--
 
 
 -- [[ Configure Treesitter ]]
